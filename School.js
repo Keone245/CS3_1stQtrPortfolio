63143fd70d489c14bca47ad0727ce84d1c5a8508
keone245 <b2029kdsrobles@pshs.edu.ph>
2025-08-28 16:24:24 +0800

    cringe

---
+++ School.js
@@ -0,0 +1,41 @@
+console.log("i WANT TO SLEEP");
+
+
+rl = require('readline')
+rli = rl.createInterFace(
+{
+input: process.stdin, 
+}
+
+)
+rli.on('line', give_station =>
+{
+
+function give_station(num) {
+    switch (Number(num)) {
+        case 7:
+             console.log('gma - 7')
+             break
+             case 2:
+                console.log('abs-cbn - 2')
+                break
+                case 5:
+                    first_letter = "t"
+                    console.log('${first_letter}v5' - 5)
+                    break
+                    case 13:
+                        console.log('ibc - 13')
+                        break
+                    case 4:
+                        console.log('ptv - 4')
+                        break
+                        default:
+                            console.log('Unknown station')
+                            break
+    }
+
+
+}
+})
+
+
